ed7b458ee37290724bb591cb0b03f7226927c356
Ceballos Camila <Camila.Ceballos@ait.ac.at>
2025-11-21 12:27:17 +0100

    Add green_id and green_area_sqm assignment to step2

--- step2_check_direct_overlap.py
+++ step2_check_direct_overlap.py
@@ -10,4 +10,17 @@ def classify_centroids_direct_overlap(centroids, green_zones, crs_metric=3857):
     centroids["classified"] = joined["index_right"].notnull().map(
         lambda x: "A" if x else None
     )
+
+    centroids["green_id"] = joined["index_right"]
+
+    if "green_area_sqm" not in green_zones.columns:
+        green_zones["green_area_sqm"] = green_zones.geometry.area
+
+    centroids["green_area_sqm"] = centroids["green_id"].map(
+    green_zones["green_area_sqm"]
+    )
+
+    centroids.loc[centroids["classified"] == "A", "green_id"] = centroids["green_id"]
+    centroids.loc[centroids["classified"] == "A", "green_area_sqm"] = centroids["green_area_sqm"]
+
     return centroids
